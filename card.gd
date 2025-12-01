1bccf8a4c019d23b4b64c1e2845d6ca463eec662
Mix56374 <82978103+Mix56374@users.noreply.github.com>
2025-11-30 23:59:18 -0800

    Small Updates

--- card.gd
+++ card.gd
@@ -1,33 +1,35 @@
 extends Sprite2D
 
-var lscale = scale
 var dtexture = texture
 var btexture = load('res://cards/card_back.png')
+var size = scale.y
 var zrot = 0.0
 var drag = 0.0
 var rot = 0.0
 
 func _ready():
-	set_meta("scale", scale)
+	set_meta("scale", scale.y)
 	set_meta("position", position)
 	set_meta("rotation", rotation)
+	if not dtexture:
+		var card_id = get_meta("card_id")
+		dtexture = load("res://cards/card_"+str(1+(card_id/14))+"_"+str(1+((card_id-1)%13))+".png")
+		texture = dtexture
 
 func _process(delta):
 	var target = get_meta("target")
-	var size = (int(get_meta("hover")) + int(get_meta("drag"))) * 0.01
+	var lsize = get_meta("scale") + ((float(get_meta("hover")) + float(get_meta("drag"))) * 0.01)
 	var lzrot = float(get_meta("flip")) * 2.0
 	var ldrag = float(get_meta("drag"))
+	
 	var lpos
-	if target and not ldrag:
-		lpos = target.position - position
+	if target:
+		lpos = lerp(target.position, get_meta("position"), ease(drag, 0.3)) - position
 	else:
 		lpos = get_meta("position") - position
 	var lrot = get_meta("rotation")
 	
-	lscale = (get_meta("scale") + Vector2(size, size))
-	lscale.x = lscale.x - (lscale.x * lzrot)
-	lpos = Vector2(cos(lpos.angle()), sin(lpos.angle())) * min(lpos.length(), 150.0 + (drag * 100.0))
-	zrot = lerp(zrot, lzrot, delta * 10.0)
+	lpos = Vector2(cos(lpos.angle()), sin(lpos.angle())) * min(lpos.length(), 200.0)
 	
 	if zrot > 1.0:
 		texture = btexture
@@ -35,7 +37,10 @@ func _process(delta):
 		texture = dtexture
 	
 	rot = lerp(rot, lrot, delta * 5.0)
-	drag = lerp(drag, ldrag, delta * 5.0)
+	drag = lerp(drag, ldrag, delta * 10.0)
 	rotation = rot + clamp(lpos.x/200.0, -0.85, 0.85)
-	position = lerp(position, position + lpos, delta * (10.0 + (drag * 10.0)))
-	scale = lerp(scale, lscale, delta * 10.0)
+	position = lerp(position, position + lpos, delta * (10.0 + (ease(drag, 0.1) * 15.0)))
+	zrot = lerp(zrot, lzrot, delta * 10.0)
+	size = lerp(size, lsize, delta * 15.0)
+	scale = Vector2(size, size)
+	scale.x = lerp(size, -size, zrot/2)

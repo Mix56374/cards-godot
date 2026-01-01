dd16b9e8f544f10cc17f5341fe1f3971ead2a650
Mix56374 <82978103+Mix56374@users.noreply.github.com>
2026-01-01 02:06:43 -0800

    Updated Layout and Gameplay Loop

--- scripts/card.gd
+++ scripts/card.gd
@@ -13,12 +13,14 @@ var floatf = 0.0
 var rng_offset = rng.randf_range(5.0, 8.0)
 var rng_sign = (2 * randi_range(0, 1)) - 1
 var ltarget = Vector2.ZERO
+var select = 0.0
+var losing = 0.0
 
 func _enter_tree():
 	set_meta("scale", scale.y)
 	var card_id = get_meta("card_id")
 	ftexture = load("res://cards/card_"+str(card_id)+".png")
-	set_deferred("texture", ftexture)
+	set_deferred("texture", btexture)
 	set_deferred("float", floatf)
 	
 	#var target = get_meta("target")
@@ -42,6 +44,8 @@ func _process(delta):
 	var lzrot = float(get_meta("flip")) * 2.0
 	var ldrag = float(get_meta("drag"))
 	var ltime = time.get_ticks_msec()/1000.0
+	var lselect = float(get_meta("selected"))
+	var llosing = float(get_meta("losing"))
 	
 	var lpos
 	if target != Vector2.ZERO:
@@ -61,7 +65,8 @@ func _process(delta):
 		lsize += floatf * ((cos(PI * (ltime/(rng_offset * 1.0) + 0.5)) + 1.0) * 0.005)
 		lrot += floatf * (sin((PI * ltime)/rng_offset) * rng_sign * 0.04)
 	
-	
+	losing = lerp(losing, llosing, delta * 10.0)
+	select = lerp(select, lselect, delta * 25.0)
 	rot = lerp(rot, lrot, delta * 5.0)
 	drag = lerp(drag, ldrag, delta * 10.0)
 	rotation = rot + clamp(lpos.x/200.0, -0.85, 0.85)
@@ -70,7 +75,16 @@ func _process(delta):
 	size = lerp(size, lsize, delta * 15.0)
 	scale = Vector2(size, size)
 	scale.x = lerp(size, -size, zrot/2)
-	# Fix the floating animation on lines 57 and 60 so it is integrated into the overall code better, and can smoothly be toggled on or off, and no I will NOT be fixing the magic numbers
+	
+	#if get_meta("float"):
+	offset.y = select * -150.0
+	if not get_meta("losing"):
+		material.set_shader_parameter("width", select * 10.0)
+	else:
+		material.set_shader_parameter("color", Color.CRIMSON)
+		material.set_shader_parameter("width", losing * 10.0)
+	
+	# Fix the floating animation on lines 57 and 60 so it is integrated into the overall code better, and can smoothly be toggled on or off, and no I will NOT be fixing the magic numbers -- nevermind i dont got TIME
 	
 	if zrot > 1.0:
 		texture = btexture
--- scripts/mouse.gd
+++ scripts/mouse.gd
@@ -2,13 +2,18 @@ extends Node
 
 var rng = RandomNumberGenerator.new()
 
-@onready var cards = $Cards
-@onready var nodes = $Nodes
+@onready var players = $Players
+@onready var local_player = $Players/Player
+
+@onready var timer = $Timer
+@onready var deck = $Deck
+@onready var pile = $Pile
 @onready var camera = $Camera
 var hover_card = null
 var drag = false
 
 var base_card = preload("res://scenes/card.tscn")
+var outline_shader = preload("res://shaders/outline.gdshader")
 @onready var viewport = get_viewport()
 @onready var screen = viewport.get_visible_rect()
 
@@ -16,58 +21,138 @@ func hand_repos(player):
 	var player_cards = player.get_meta("cards")
 	for i in player_cards.size():
 		var icard = player_cards[i]
-		icard.set_meta("target", Vector2((screen.size.x - ((player_cards.size() - 1) * 115))/2 + (i * 115), screen.size.y - 150))
+		icard.set_meta("target", Vector2((screen.size.x - ((player_cards.size() - 1.0) * 115.0))/2.0 + (i * 115.0), screen.size.y - 150.0))
 
 func new_card(card_id, player):
 	var card = base_card.instantiate()
+	card.material = ShaderMaterial.new()
+	card.material.shader = outline_shader.duplicate(true)
 	card.set_meta("card_id", card_id)
-	card.set_meta("interactable", true)
-	card.position = cards.position
+	card.position = deck.position
 	#card.position = screen.size - (card.get_rect().size * card.scale)/2 - Vector2(40.0 - (i/10), 40.0 - (i/10))
 	#card.set_meta("rotation", rng.randf_range(-0.02, 0.02))
-	cards.add_child(card)
+	deck.add_child(card)
 	var player_cards = player.get_meta("cards")
 	player_cards.append(card)
 	
+	if player == local_player:
+		card.set_meta("flip", false)
+	card.set_meta("float", true)
+	
+	player_cards.sort_custom(
+		func sort_cards(a, b):
+			return a.get_meta("card_id") < b.get_meta("card_id")
+	)
+	hand_repos(player)
+
+func play_card(card, player):
+	card.remove_from_group("Idle")
+	for idle_card in get_tree().get_nodes_in_group("Idle"):
+		if idle_card.get_meta("card_id") <= card.get_meta("card_id"):
+			var losing_cards = players.get_meta("losing_cards")
+			losing_cards.append(idle_card)
+			idle_card.set_meta("flip", false)
+			idle_card.set_meta("losing", true)
+	
+	card.z_index = card.get_meta("card_id")
 	card.set_meta("flip", false)
+	card.set_meta("float", false)
+	card.set_meta("selected", false)
+	card.set_meta("scale", 0.25)
+	card.set_meta("rotation", rng.randf_range(-0.15, 0.15))
+	card.set_meta("target", pile.position)
+	#card.reparent(pile)
+	
+	var player_cards = player.get_meta("cards")
+	player_cards.erase(card)
 	hand_repos(player)
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			if event.is_pressed() and hover_card:
-				drag = hover_card.position - get_viewport().get_mouse_position()
-				hover_card.set_meta("drag", true)
-			elif event.is_released() and drag:
-				hover_card.set_meta("drag", false)
-				drag = false
-		elif event.button_index == MOUSE_BUTTON_RIGHT:
-			if event.is_pressed() and hover_card:
-				hover_card.set_meta("flip", not hover_card.get_meta("flip"))
-		elif event.button_index == MOUSE_BUTTON_MIDDLE:
-			if event.is_pressed():
-				new_card(rng.randi_range(1, 52), get_tree().get_root().get_node("World").get_node("Players").get_node("Player"))
+				hover_card.set_meta("selected", not hover_card.get_meta("selected"))
+				
+				var selected_cards = local_player.get_meta("selected_cards")
+				if hover_card.get_meta("selected"):
+					selected_cards.append(hover_card)
+				elif selected_cards.has(hover_card):
+					selected_cards.erase(hover_card)
+		
+		#elif event.button_index == MOUSE_BUTTON_RIGHT:
+			#if event.is_pressed() and hover_card:
+				#hover_card.set_meta("flip", not hover_card.get_meta("flip"))
+		
+		#elif event.button_index == MOUSE_BUTTON_MIDDLE:
+			#if event.is_pressed():
+				#new_card(rng.randi_range(1, 52), local_player)
 			#if event.is_pressed() and hover_card:
 				#hover_card.set_meta("float", not hover_card.get_meta("float"))
+	
+	elif event is InputEventKey:
+		var player_cards = local_player.get_meta("cards")
+		var selected_cards = local_player.get_meta("selected_cards")
+		
+		if event.is_action_pressed("play") and selected_cards.size() > 0:
+			local_player.set_meta("selected_cards", [])
+			selected_cards.sort_custom(
+				func sort_cards(a, b):
+					return a.get_meta("card_id") < b.get_meta("card_id")
+			)
+			
+			for card in player_cards:
+				card.set_meta("interactable", false)
+			
+			timer.start()
+			for card in selected_cards:
+				play_card(card, local_player)
+				if players.get_meta("losing_cards"):
+					break
+				await timer.timeout
+			timer.stop()
+			
+			if not players.get_meta("losing_cards"):
+				for card in player_cards:
+					card.set_meta("interactable", true)
+
+func card_exists(card_id):
+	for card in get_tree().get_nodes_in_group("Cards"):
+		if card.get_meta("card_id") == card_id:
+			return true
+	return false
+
+func _ready():
+	timer.start()
+	for i in range(8):
+		var card_id = randi_range(1, 52)
+		while card_exists(card_id):
+			card_id = randi_range(1, 52)
+		new_card(card_id, local_player)
+		
+		await timer.timeout
+	timer.stop()
+	
+	for card in local_player.get_meta("cards"):
+				card.set_meta("interactable", true)
 
 func _process(_delta):
 	var mouse = viewport.get_mouse_position()
 	
 	mouse = mouse.clamp(screen.position, screen.end) + camera.position
 	
-	if drag:
-		hover_card.set_meta("position", mouse + drag)
-		
-		for node in nodes.get_children():
-			if node.get_node("Collision").get_shape().get_rect().has_point(node.to_local(mouse)):
-				hover_card.set_meta("target", node)
-		
-	else:
-		var card_nodes = get_tree().get_nodes_in_group("Cards")
-		hover_card = null
-		for card in card_nodes:
-			if card.get_meta("interactable") and card.get_rect().has_point(card.to_local(mouse)):
-				hover_card = card
-		
-		for card in card_nodes:
-			card.set_meta("hover", card == hover_card)
+	#if drag:
+		#hover_card.set_meta("position", mouse + drag)
+		#
+		#for node in nodes.get_children():
+			#if node.get_node("Collision").get_shape().get_rect().has_point(node.to_local(mouse)):
+				#hover_card.set_meta("target", node)
+		#
+	#else:
+	var card_nodes = get_tree().get_nodes_in_group("Cards")
+	hover_card = null
+	for card in card_nodes:
+		if card.get_meta("interactable") and card.get_rect().has_point(card.to_local(mouse)):
+			hover_card = card
+	
+	for card in card_nodes:
+		card.set_meta("hover", card == hover_card)
